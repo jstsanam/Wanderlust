d191eae4226964d18cf19d5cb888474d839df20d
jstsanam <jstsanam@gmail.com>
2024-05-25 21:32:49 +0530

    added mongo store for storing sessions in mongo atlas

--- middlewares/sessionMiddleware.js
+++ middlewares/sessionMiddleware.js
@@ -1,10 +1,21 @@
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import dotenv from "dotenv";
 dotenv.config();
 
 const sessionDuration = 7 * 24 * 60 * 60 * 1000;
 
+const mongoStore = MongoStore.create({
+  mongoUrl: process.env.DATABASE_URL,
+  crypto: {
+    secret: process.env.SESSION_SECRET_CODE
+  },
+  // touchAfter is interval (in seconds) between session updates
+  touchAfter: 24 * 3600
+})
+
 const sessionMiddleware = session({
+    store: mongoStore,
     secret: process.env.SESSION_SECRET_CODE,
     resave: false,
     saveUninitialized: true,
